fe9c2c6c51cff78cff61916c1d41f55f5f1acc5f
Alejandra Lopez Fuentes <alejandra.lopez@travelperk.com>
2023-05-12 12:59:13 +0200

    recipe modle creation

---
+++ app/recipe/tests/test_recipe_api.py
@@ -0,0 +1,6 @@
+"""
+Tests for recipe APIs.
+"""
+from decimal import Decimal
+import tempfile
+import os
---
+++ app/recipe/urls.py
@@ -0,0 +1,12 @@
+"""
+URL mappings for the recipe API.
+"""
+from django.urls import path
+
+from user import views
+
+app_name = 'recipe'
+
+urlpatterns = [
+
+]
