4e39a3b98ce90c9baf49ee945356bdc774ca78a0
Alejandra Lopez Fuentes <alejandra.lopez@travelperk.com>
2023-05-17 12:39:27 +0200

    1st commit: basic recipe exercise

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,8 +19,6 @@ from drf_spectacular.views import (
 )
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
 
 
 urlpatterns = [
@@ -34,9 +32,3 @@ urlpatterns = [
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT,
-    )
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,7 +1,6 @@
 """
 Tests for models.
 """
-from unittest.mock import patch
 from decimal import Decimal
 
 from django.test import TestCase
@@ -66,18 +65,9 @@ class ModelTests(TestCase):
         recipe = models.Recipe.objects.create(
             user=user,
             title='Sample recipe name',
-            time_minutes=5,
-            price=Decimal('5.50'),
-            description='Sample receipe description.',
+            description='Sample recipe description.',
         )
         self.assertEqual(str(recipe), recipe.title)
-
-    def test_create_tag(self):
-        """Test creating a tag is successful."""
-        user = create_user()
-        tag = models.Tag.objects.create(user=user, name='Tag1')
-
-        self.assertEqual(str(tag), tag.name)
 
     def test_create_ingredient(self):
         """Test creating an ingredient is successful."""
@@ -89,11 +79,3 @@ class ModelTests(TestCase):
 
         self.assertEqual(str(ingredient), ingredient.name)
 
-    @patch('core.models.uuid.uuid4')
-    def test_recipe_file_name_uuid(self, mock_uuid):
-        """Test generating image path."""
-        uuid = 'test-uuid'
-        mock_uuid.return_value = uuid
-        file_path = models.recipe_image_file_path(None, 'example.jpg')
-
-        self.assertEqual(file_path, f'uploads/recipe/{uuid}.jpg')
